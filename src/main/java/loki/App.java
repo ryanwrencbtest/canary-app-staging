8aff5c093fbec84b65cda80db59772a80e14411f
ryanwren <rwren@cloudbees.com>
2017-12-11 15:09:04 -0500

    SAMPLE-1 Test commit for the Checkout app.

--- src/main/java/loki/App.java
+++ src/main/java/loki/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-	System.out.println( "This is the Loki App" );
+	System.out.println( "This is the Loki App!!" );
     }
 }
